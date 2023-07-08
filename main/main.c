28452b54621fd1758c2432984766eefba8e27ef7
Saurabh Kdam <ssk3@somaiya.edu>
2023-07-08 18:00:22 +0530

    incomplete work save 17:59 08-07-23

--- main/main.c
+++ main/main.c
@@ -17,6 +17,59 @@ typedef struct
 
 } WIFI_CREDENTIALS_t;
 
+/*
+ * This fuction is for the save the wifi credentials into the NVS
+ */
+static esp_err_t save_the_wifi_credentials_into_NVS(WIFI_CREDENTIALS_t *wifi_credentials)
+{
+    nvs_handle NVS_handler;
+
+    ESP_ERROR_CHECK(nvs_open("my_credentials", NVS_READWRITE, &NVS_handler));
+
+    ESP_ERROR_CHECK(nvs_set_blob(NVS_handler, "Credentials", wifi_credentials, sizeof(WIFI_CREDENTIALS_t)));
+
+    ESP_ERROR_CHECK(nvs_commit(NVS_handler));
+
+    nvs_close(NVS_handler);
+
+    return ESP_OK;
+}
+
+/*
+ * This fuction is for the read the wifi credentials from the NVS
+ */
+static esp_err_t read_the_wifi_credentials_from_NVS(WIFI_CREDENTIALS_t *wifi_credentials)
+{
+    nvs_handle NVS_handler;
+
+    nvs_open("my_credentials", NVS_READWRITE, &NVS_handler);
+
+    size_t i = sizeof(WIFI_CREDENTIALS_t);
+
+    esp_err_t result = nvs_get_blob(NVS_handler, "Credentials", wifi_credentials, &i);
+
+    switch (result)
+    {
+
+    case ESP_ERR_NVS_NOT_FOUND:
+    case ESP_ERR_NOT_FOUND:
+        ESP_LOGE(TAG, "Value not set yet");
+        break;
+
+    case ESP_OK:
+        ESP_LOGI(TAG, "Value is set");
+        break;
+
+    default:
+        ESP_LOGE(TAG, "Error (%s) opening NVS handle!\n", esp_err_to_name(result));
+        break;
+    }
+
+    nvs_close(NVS_handler);
+
+    return result;
+}
+
 static esp_err_t set_wifi_credentials_url(httpd_req_t *req)
 {
     ESP_LOGI(TAG, "URL: %s", req->uri);
@@ -46,17 +99,7 @@ static esp_err_t save_wifi_credentials_url(httpd_req_t *req)
 
     printf("SSID is - %s \n SSID size of the string is %d \n PASSWORD - %s \n Password size of the string is %d \n", wifi_credentials_received_from_WEB_page.WIFI_SSID, strlen(wifi_credentials_received_from_WEB_page.WIFI_SSID), wifi_credentials_received_from_WEB_page.WIFI_PASSWORD, strlen(wifi_credentials_received_from_WEB_page.WIFI_PASSWORD));
 
-    /////////////////////////////////////////////////////////
-    nvs_handle NVS_handler;
-
-    ESP_ERROR_CHECK(nvs_open("my_credentials", NVS_READWRITE, &NVS_handler));
-
-    ESP_ERROR_CHECK(nvs_set_blob(NVS_handler, "Credentials", &wifi_credentials_received_from_WEB_page, sizeof(WIFI_CREDENTIALS_t)));
-
-    ESP_ERROR_CHECK(nvs_commit(NVS_handler));
-
-    nvs_close(NVS_handler);
-    /////////////////////////////////////////////////////////
+    save_the_wifi_credentials_into_NVS(&wifi_credentials_received_from_WEB_page);
 
     httpd_resp_sendstr(req, save_wifi_credentials_HTML);
     return ESP_OK;
@@ -86,31 +129,13 @@ void app_main(void)
 {
     ESP_ERROR_CHECK(nvs_flash_init());
 
-    nvs_handle NVS_handler;
-    nvs_open("my_credentials", NVS_READWRITE, &NVS_handler);
-
     WIFI_CREDENTIALS_t wifi_credentials_read_from_NVS;
 
-    size_t i = sizeof(WIFI_CREDENTIALS_t);
-
-    esp_err_t result = nvs_get_blob(NVS_handler, "Credentials", &wifi_credentials_read_from_NVS, &i);
-
-    switch (result)
+    if (read_the_wifi_credentials_from_NVS(&wifi_credentials_read_from_NVS) == ESP_OK)
     {
-    case ESP_ERR_NVS_NOT_FOUND:
-    case ESP_ERR_NOT_FOUND:
-        ESP_LOGE(TAG, "Value not set yet");
-        break;
-    case ESP_OK:
-        ESP_LOGI(TAG, "Value is set");
-        break;
-    default:
-        ESP_LOGE(TAG, "Error (%s) opening NVS handle!\n", esp_err_to_name(result));
-        break;
     }
-    nvs_close(NVS_handler);
 
-    printf("SSID is - %s \n SSID size of the string is %d \n PASSWORD - %s \n Password size of the string is %d \n", wifi_credentials_read_from_NVS.WIFI_SSID, strlen(wifi_credentials_read_from_NVS.WIFI_SSID), wifi_credentials_read_from_NVS.WIFI_PASSWORD, strlen(wifi_credentials_read_from_NVS.WIFI_PASSWORD));
+    printf(" SSID is - %s \n SSID size of the string is %d \n PASSWORD - %s \n Password size of the string is %d \n", wifi_credentials_read_from_NVS.WIFI_SSID, strlen(wifi_credentials_read_from_NVS.WIFI_SSID), wifi_credentials_read_from_NVS.WIFI_PASSWORD, strlen(wifi_credentials_read_from_NVS.WIFI_PASSWORD));
 
     wifi_init();
     wifi_connect_ap("POCO", "password");
